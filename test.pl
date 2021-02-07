f81419a9172094b7c64dfed6cfa6acb14d28fd9b
Christian Kobril <christian.kobril@studenti.fauser.edu>
2021-02-08 00:21:16 +0100

    Quasi ultimato algoritmo di Prim, sistemati valori uguali e sostituzione di valori a parità di nome del vertice in Heapify. Non funziona adeguatamente vertex_previous

--- test.pl
+++ test.pl
@@ -3,18 +3,18 @@
 
 % heap di test - ben formato
 
-heap(myHeap, 11).
-heap_entry(myHeap, 0, 3, 3).
-heap_entry(myHeap, 1, 18, 18).
-heap_entry(myHeap, 2, 5, 5).
-heap_entry(myHeap, 3, 24, 24).
-heap_entry(myHeap, 4, 19, 19).
-heap_entry(myHeap, 5, 14, 14).
-heap_entry(myHeap, 6, 15, 15).
-heap_entry(myHeap, 7, 25, 25).
-heap_entry(myHeap, 8, 32, 32).
-heap_entry(myHeap, 9, 20, 20).
-heap_entry(myHeap, 10, 22, 22).
+%heap(myHeap, 11).
+%heap_entry(myHeap, 0, 3, 3).
+%heap_entry(myHeap, 1, 18, 18).
+%heap_entry(myHeap, 2, 5, 5).
+%heap_entry(myHeap, 3, 24, 24).
+%heap_entry(myHeap, 4, 19, 19).
+%heap_entry(myHeap, 5, 14, 14).
+%heap_entry(myHeap, 6, 15, 15).
+%heap_entry(myHeap, 7, 25, 25).
+%heap_entry(myHeap, 8, 32, 32).
+%heap_entry(myHeap, 9, 20, 20).
+%heap_entry(myHeap, 10, 22, 22).
 
 % grafo di test
 
